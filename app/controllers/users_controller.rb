1d4d09033f69ea6ae2097f741f17004d67a02154
satoru shimono <michael513.sato@gmail.com>
2018-11-18 16:16:42 +0900

    Set username as param

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -66,7 +66,8 @@ class UsersController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_user
-      @user = User.find(params[:id])
+      # @user = User.find(params[:username])
+      @user = User.find_by(username: params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,8 @@ class User < ApplicationRecord
 
   # validates :username, presence: true, uniqueness: true
   validates :email, presence: true, uniqueness: true
+
+  def to_param
+    username
+  end
 end
