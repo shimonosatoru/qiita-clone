259fbd515caef9147e639c575a6c06b5f6e3e216
satoru shimono <michael513.sato@gmail.com>
2018-11-18 15:05:19 +0900

    Add validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :articles, dependent: :destroy
+
+  # validates :username, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true
 end
